7b9fedb75c324a9e64992b3f7e41a2144fc7f7f4
romulo-souza <134983740+romulo-souza@users.noreply.github.com>
2025-09-18 17:23:15 -0300

    atualização script de captura de tempo de inicialização

--- Scripts_Python/Tempo_Ini/scriptCapturaTempoIni.py
+++ Scripts_Python/Tempo_Ini/scriptCapturaTempoIni.py
@@ -8,9 +8,10 @@ ATIVIDADE = ".MainActivity"
 NUM_EXPERIMENTOS = 3
 CSV_OUTPUT = "resultados_inicializacao.csv"
 
-def executar_comando(cmd):
+def executar_comando(cmd, delay=2):
     #Executa um comando no terminal e retorna a saída
     resultado = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(delay)
     return resultado.stdout.decode("utf-8")
 
 def extrair_total_time(saida):
@@ -18,6 +19,7 @@ def extrair_total_time(saida):
     for linha in saida.splitlines():
         if "TotalTime" in linha:
             valor = linha.split(":")[1].strip()
+            print(saida)
             return int(valor)
     return None
 
@@ -49,26 +51,22 @@ def main():
         resultados.append((i, "cold", tempo))
         # Fecha o app (botão voltar)
         pressionar_botao(4)
-        time.sleep(2)
-
+        
         # Warm Start
         tempo = executar_warm_start()
         resultados.append((i, "warm", tempo))
         pressionar_botao(4)
-        time.sleep(2)
-
+        
         # Hot Start
         executar_warm_start()  # abre o app
         pressionar_botao(3)    # botão Home (vai pra segundo plano)
-        time.sleep(1)
         tempo = executar_hot_start()
         resultados.append((i, "hot", tempo))
         pressionar_botao(4)    # fecha o app
-        time.sleep(2)
 
         print(f"Experimento {i} concluído.")
 
-    # Salva resultados em CSV
+    # Salva CSV
     with open(CSV_OUTPUT, "w", newline="", encoding="utf-8") as f:
         escritor = csv.writer(f)
         escritor.writerow(["experimento", "tipo_inicializacao", "tempo"])
